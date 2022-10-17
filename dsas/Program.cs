8d10e21422a8f26adc7d2bd4b8dbd92c1df0cbae
test <test@KAB42-1>
2022-10-17 11:47:07 +0300

    Добавьте файлы проекта.

---
+++ dsas/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("Введите.... Что то введите короче.");
+string a=Console.ReadLine();
+char[] s = a.ToCharArray();
+int count = 0;
+int maxcount = 0;
+foreach (char c in s)
+{
+    if (char.IsDigit(c))
+    {
+        count++;       
+        if (maxcount <= count)
+        {
+            maxcount = count;
+        }
+    }
+    else if(char.IsDigit(c) == false)
+    {
+        count = 0;
+        continue;
+    } 
+}
+Console.WriteLine($"Максимальная последовательность чисел - {maxcount}");
